b4860e041dee32793d5efc25e9d2d52cee97cbe0
Abberate <abdoulaye222.ab@gmail.com>
2024-06-28 10:14:26 +0100

    28-06-2024-commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +8,42 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+/*  Future<void> getLocation() async {
+    // Check and request location permission if not granted
+    if (await _checkLocationPermission()) {
+      try {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.low);
+        print(position);
+      } catch (e) {
+        print("Error getting location: $e");
+        // Handle error getting location
+      }
+    } else {
+      // Handle case when location permission is not granted
+      // You can show a dialog or a snackbar to inform the user
+      // and provide them with an option to grant the permission.
+      // For simplicity, we are printing an error message here.
+      print("Location permission denied.");
+    }
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    if (await Permission.location.isGranted) {
+      return true;
+    } else {
+      // Request permission
+      var status = await Permission.location.request();
+      return status.isGranted;
+    }
+  }*/
+
+  void getLocation() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +51,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
